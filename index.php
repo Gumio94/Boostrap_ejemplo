e9042458e6ffba804022692b575b0f7a6239286a
Nicolas <53067913+Gumio94@users.noreply.github.com>
2020-06-30 20:28:37 -0300

    mod

--- index.php
+++ index.php
@@ -29,7 +29,24 @@
       </div>
     </div>
    
+    <div class="container-fluid">
+      <div class="row fila py-2">
+                      
 
+        <div class="col-sm-6 col d-md-block d-sm-none">
+          One of three columns
+        </div>
+         <div class="d-md-none col-sm-6 col">
+          dasdasdsad
+        </div>
+        <div class="col-sm-1 col">
+          One of three columns
+        </div>
+        <div class="col-sm-1 col">
+          One of three columns
+        </div>
+      </div>
+    </div>
     <script src="js/jquery-3.5.1.js" ></script>
     <script src="js/popper.js" ></script>
     <script src="js/bootstrap.min.js"></script>
