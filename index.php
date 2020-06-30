b650deeb82040f9892394342822e999c534762df
NicolasAndrada <nicolas.andrada2@gmail.com>
2020-06-30 20:25:14 -0300

    boos

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!--estilos--> 
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/miestilo.css">
+    
+</head>
+<body>
+
+   <div class="container-fluid">
+      <div class="row fila py-2">
+                      
+
+        <div class="col-sm-6 col d-md-block d-sm-none">
+          One of three columns
+        </div>
+         <div class="d-md-none col-sm-6 col">
+          dasdasdsad
+        </div>
+        <div class="col-sm-1 col">
+          One of three columns
+        </div>
+        <div class="col-sm-1 col">
+          One of three columns
+        </div>
+      </div>
+    </div>
+   
+
+    <script src="js/jquery-3.5.1.js" ></script>
+    <script src="js/popper.js" ></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+</body>
+</html>
